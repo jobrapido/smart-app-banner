42657f52442503151c5186fb53379bbf5f665f36
fabio <fabio.arcari@jobrapido.com>
2016-10-24 11:28:12 +0200

    MOB-441: add icon option

--- smart-app-banner.js
+++ smart-app-banner.js
@@ -1,257 +1,320 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SmartBanner = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var extend = require('xtend/mutable');
-var q = require('component-query');
-var doc = require('get-doc');
-var root = doc && doc.documentElement;
-var cookie = require('cookie-cutter');
+(function (f) {
+  if (typeof exports === "object" && typeof module !== "undefined") {
+    module.exports = f()
+  } else if (typeof define === "function" && define.amd) {
+    define([], f)
+  } else {
+    var g;
+    if (typeof window !== "undefined") {
+      g = window
+    } else if (typeof global !== "undefined") {
+      g = global
+    } else if (typeof self !== "undefined") {
+      g = self
+    } else {
+      g = this
+    }
+    g.SmartBanner = f()
+  }
+})(function () {
+  var define, module, exports;
+  return (function e(t, n, r) {
+    function s(o, u) {
+      if (!n[o]) {
+        if (!t[o]) {
+          var a = typeof require == "function" && require;
+          if (!u && a) return a(o, !0);
+          if (i) return i(o, !0);
+          var f = new Error("Cannot find module '" + o + "'");
+          throw f.code = "MODULE_NOT_FOUND", f
+        }
+        var l = n[o] = {
+          exports: {}
+        };
+        t[o][0].call(l.exports, function (e) {
+          var n = t[o][1][e];
+          return s(n ? n : e)
+        }, l, l.exports, e, t, n, r)
+      }
+      return n[o].exports
+    }
+    var i = typeof require == "function" && require;
+    for (var o = 0; o < r.length; o++) s(r[o]);
+    return s
+  })({
+    1: [function (require, module, exports) {
+      var extend = require('xtend/mutable');
+      var q = require('component-query');
+      var doc = require('get-doc');
+      var root = doc && doc.documentElement;
+      var cookie = require('cookie-cutter');
 
 
-// platform dependent functionality
-var mixins = {
-	ios: {
-		appMeta: 'apple-itunes-app-meta',
-		iconRels: ['apple-touch-icon-precomposed', 'apple-touch-icon'],
-		getStoreLink: function() {
-			return this.options.url.ios;
-		}
-	},
-	android: {
-		appMeta: 'google-play-app',
-		iconRels: ['android-touch-icon', 'apple-touch-icon-precomposed', 'apple-touch-icon'],
-		getStoreLink: function() {
-			return this.options.url.android;
-		}
-	}
-};
+      // platform dependent functionality
+      var mixins = {
+        ios: {
+          appMeta: 'apple-itunes-app-meta',
+          iconRels: ['apple-touch-icon-precomposed', 'apple-touch-icon'],
+          getStoreLink: function () {
+            return this.options.url.ios;
+          }
+        },
+        android: {
+          appMeta: 'google-play-app',
+          iconRels: ['android-touch-icon', 'apple-touch-icon-precomposed', 'apple-touch-icon'],
+          getStoreLink: function () {
+            return this.options.url.android;
+          }
+        }
+      };
 
-var SmartBanner = function(options) {
-	var userAgent = navigator.userAgent;
-	this.options = extend({}, {
-		daysHidden: 15,
-		daysReminder: 90,
-		appStoreLanguage: 'us', // Language code for App Store
-		button: 'OPEN', // Text for the install button
-		store: {
-			ios: 'On the App Store',
-			android: 'In Google Play'
-		},
-		price: {
-			ios: 'FREE',
-			android: 'FREE'
-		},
-		force: false // put platform type (ios, android, etc.) here for emulation
-	}, options || {});
+      var SmartBanner = function (options) {
+        var userAgent = navigator.userAgent;
+        this.options = extend({}, {
+          daysHidden: 15,
+          daysReminder: 90,
+          appStoreLanguage: 'us', // Language code for App Store
+          button: 'OPEN', // Text for the install button
+          store: {
+            ios: 'On the App Store',
+            android: 'In Google Play'
+          },
+          price: {
+            ios: 'FREE',
+            android: 'FREE'
+          },
+          force: false // put platform type (ios, android, etc.) here for emulation
+        }, options || {});
 
-	if (this.options.force) {
-		this.type = this.options.force;
-	} else if (userAgent.match(/iPad|iPhone|iPod/i) !== null) {
-		if (userAgent.match(/Safari/i) !== null ||
-				(userAgent.match(/CriOS/i) !== null ||
-				Number(userAgent.substr(userAgent.indexOf('OS ') + 3, 3).replace('_', '.')) < 6)) {
-			this.type = 'ios';
-		} // Check webview and native smart banner support (iOS 6+)
-	} else if (userAgent.match(/Android/i) !== null) {
-		this.type = 'android';
-	}
+        if (this.options.force) {
+          this.type = this.options.force;
+        } else if (userAgent.match(/iPad|iPhone|iPod/i) !== null) {
+          if (userAgent.match(/Safari/i) !== null ||
+            (userAgent.match(/CriOS/i) !== null ||
+              Number(userAgent.substr(userAgent.indexOf('OS ') + 3, 3)
+                .replace('_', '.')) < 6)) {
+            this.type = 'ios';
+          } // Check webview and native smart banner support (iOS 6+)
+        } else if (userAgent.match(/Android/i) !== null) {
+          this.type = 'android';
+        }
 
-	// Don't show banner if device isn't iOS or Android, website is loaded in app, user dismissed banner, or we have no app id in meta
-	if (!this.type
-		|| navigator.standalone
-		|| cookie.get('smartbanner-closed')
-		|| cookie.get('smartbanner-installed')) {
-		return;
-	}
+        // Don't show banner if device isn't iOS or Android, website is loaded in app, user dismissed banner, or we have no app id in meta
+        if (!this.type || navigator.standalone || cookie.get('smartbanner-closed') || cookie.get('smartbanner-installed')) {
+          return;
+        }
 
-	extend(this, mixins[this.type]);
+        extend(this, mixins[this.type]);
 
-	if (!this.parseAppId()) {
-		return;
-	}
+        if (!this.parseAppId()) {
+          return;
+        }
 
-	this.create();
-	this.show();
-};
+        this.create();
+        this.show();
+      };
 
-SmartBanner.prototype = {
-	constructor: SmartBanner,
+      SmartBanner.prototype = {
+        constructor: SmartBanner,
 
-	create: function() {
-		var link = this.getStoreLink();
-		var inStore = this.options.price[this.type] + ' - ' + this.options.store[this.type];
-		var icon;
-		for (var i = 0; i < this.iconRels.length; i++) {
-			var rel = q('link[rel="'+this.iconRels[i]+'"]');
-			if (rel) {
-				icon = rel.getAttribute('href');
-				break;
-			}
-		}
+        create: function () {
+          var link = this.getStoreLink();
+          var inStore = this.options.price[this.type] + ' - ' + this.options.store[this.type];
+          var icon;
+          if (this.options.icon) {
+            icon = this.options.icon;
+          } else {
+            for (var i = 0; i < this.iconRels.length; i++) {
+              var rel = q('link[rel="' + this.iconRels[i] + '"]');
+              if (rel) {
+                icon = rel.getAttribute('href');
+                break;
+              }
+            }
+          }
 
-		var sb = doc.createElement('div');
-		sb.className = 'smartbanner smartbanner-' + this.type;
+          var sb = doc.createElement('div');
+          sb.className = 'smartbanner smartbanner-' + this.type;
 
-		sb.innerHTML = '<div class="smartbanner-container">' +
-							'<a href="javascript:void(0);" class="smartbanner-close">&times;</a>' +
-							'<span class="smartbanner-icon" style="background-image: url('+icon+')"></span>' +
-							'<div class="smartbanner-info">' +
-								'<div class="smartbanner-title">'+this.options.title+'</div>' +
-								'<div>'+this.options.author+'</div>' +
-								'<span>'+inStore+'</span>' +
-							'</div>' +
-							'<a href="'+link+'" class="smartbanner-button">' +
-								'<span class="smartbanner-button-text">'+this.options.button+'</span>' +
-							'</a>' +
-						'</div>';
+          sb.innerHTML = '<div class="smartbanner-container">' +
+            '<a href="javascript:void(0);" class="smartbanner-close">&times;</a>' +
+            '<span class="smartbanner-icon" style="background-image: url(' + icon + ')"></span>' +
+            '<div class="smartbanner-info">' +
+            '<div class="smartbanner-title">' + this.options.title + '</div>' +
+            '<div>' + this.options.author + '</div>' +
+            '<span>' + inStore + '</span>' +
+            '</div>' +
+            '<a href="' + link + '" class="smartbanner-button">' +
+            '<span class="smartbanner-button-text">' + this.options.button + '</span>' +
+            '</a>' +
+            '</div>';
 
-		//there isn’t neccessary a body
-		if (doc.querySelector('.body-wrapper')) {
-			doc.querySelector('.body-wrapper').appendChild(sb);
-			doc.body.classList.add('smart-banner');
-		}
-		else if (doc) {
-			doc.addEventListener('DOMContentLoaded', function(){
-				doc.querySelector('.body-wrapper').appendChild(sb);
-				doc.body.classList.add('smart-banner');
-			});
-		}
+          //there isn’t neccessary a body
+          if (doc.querySelector('.body-wrapper')) {
+            doc.querySelector('.body-wrapper')
+              .appendChild(sb);
+            doc.body.classList.add('smart-banner');
+          } else if (doc) {
+            doc.addEventListener('DOMContentLoaded', function () {
+              doc.querySelector('.body-wrapper')
+                .appendChild(sb);
+              doc.body.classList.add('smart-banner');
+            });
+          }
 
-		q('.smartbanner-button', sb).addEventListener('click', this.install.bind(this), false);
-		q('.smartbanner-close', sb).addEventListener('click', this.close.bind(this), false);
+          q('.smartbanner-button', sb)
+            .addEventListener('click', this.install.bind(this), false);
+          q('.smartbanner-close', sb)
+            .addEventListener('click', this.close.bind(this), false);
 
-	},
-	hide: function() {
-		root.classList.remove('smartbanner-show');
-	},
-	show: function() {
-		root.classList.add('smartbanner-show');
-	},
-	close: function() {
-		this.hide();
-		doc.body.classList.remove('smart-banner');
-		if(this.options.daysHidden > 0) {
-			cookie.set('smartbanner-closed', 'true', {
-				path: '/',
-				expires: +new Date() + this.options.daysHidden * 1000 * 60 * 60 * 24
-			});
-		}
-	},
-	install: function() {
-		this.hide();
-		doc.body.classList.remove('smart-banner');
-		if(this.options.daysReminder > 0) {
-			cookie.set('smartbanner-installed', 'true', {
-				path: '/',
-				expires: +new Date() + this.options.daysReminder * 1000 * 60 * 60 * 24
-			});
-		}
-	},
-	parseAppId: function() {
-		var meta = q('meta[name="' + this.appMeta + '"]');
-		if (!meta) {
-			return;
-		}
+        },
+        hide: function () {
+          root.classList.remove('smartbanner-show');
+        },
+        show: function () {
+          root.classList.add('smartbanner-show');
+        },
+        close: function () {
+          this.hide();
+          doc.body.classList.remove('smart-banner');
+          if (this.options.daysHidden > 0) {
+            cookie.set('smartbanner-closed', 'true', {
+              path: '/',
+              expires: +new Date() + this.options.daysHidden * 1000 * 60 * 60 * 24
+            });
+          }
+        },
+        install: function () {
+          this.hide();
+          doc.body.classList.remove('smart-banner');
+          if (this.options.daysReminder > 0) {
+            cookie.set('smartbanner-installed', 'true', {
+              path: '/',
+              expires: +new Date() + this.options.daysReminder * 1000 * 60 * 60 * 24
+            });
+          }
+        },
+        parseAppId: function () {
+          var meta = q('meta[name="' + this.appMeta + '"]');
+          if (!meta) {
+            return;
+          }
 
-		if (this.type === 'windows') {
-			this.appId = meta.getAttribute('content');
-		} else {
-			this.appId = /app-id=([^\s,]+)/.exec(meta.getAttribute('content'))[1];
-		}
+          if (this.type === 'windows') {
+            this.appId = meta.getAttribute('content');
+          } else {
+            this.appId = /app-id=([^\s,]+)/.exec(meta.getAttribute('content'))[1];
+          }
 
-		return this.appId;
-	}
-};
+          return this.appId;
+        }
+      };
 
-module.exports = SmartBanner;
+      module.exports = SmartBanner;
 
-},{"component-query":2,"cookie-cutter":3,"get-doc":4,"xtend/mutable":6}],2:[function(require,module,exports){
-function one(selector, el) {
-  return el.querySelector(selector);
-}
+    }, {
+      "component-query": 2,
+      "cookie-cutter": 3,
+      "get-doc": 4,
+      "xtend/mutable": 6
+    }],
+    2: [function (require, module, exports) {
+      function one(selector, el) {
+        return el.querySelector(selector);
+      }
 
-exports = module.exports = function(selector, el){
-  el = el || document;
-  return one(selector, el);
-};
+      exports = module.exports = function (selector, el) {
+        el = el || document;
+        return one(selector, el);
+      };
 
-exports.all = function(selector, el){
-  el = el || document;
-  return el.querySelectorAll(selector);
-};
+      exports.all = function (selector, el) {
+        el = el || document;
+        return el.querySelectorAll(selector);
+      };
 
-exports.engine = function(obj){
-  if (!obj.one) throw new Error('.one callback required');
-  if (!obj.all) throw new Error('.all callback required');
-  one = obj.one;
-  exports.all = obj.all;
-  return exports;
-};
+      exports.engine = function (obj) {
+        if (!obj.one) throw new Error('.one callback required');
+        if (!obj.all) throw new Error('.all callback required');
+        one = obj.one;
+        exports.all = obj.all;
+        return exports;
+      };
 
-},{}],3:[function(require,module,exports){
-var exports = module.exports = function (doc) {
-    if (!doc) doc = {};
-    if (typeof doc === 'string') doc = { cookie: doc };
-    if (doc.cookie === undefined) doc.cookie = '';
+    }, {}],
+    3: [function (require, module, exports) {
+      var exports = module.exports = function (doc) {
+        if (!doc) doc = {};
+        if (typeof doc === 'string') doc = {
+          cookie: doc
+        };
+        if (doc.cookie === undefined) doc.cookie = '';
 
-    var self = {};
-    self.get = function (key) {
-        var splat = doc.cookie.split(/;\s*/);
-        for (var i = 0; i < splat.length; i++) {
+        var self = {};
+        self.get = function (key) {
+          var splat = doc.cookie.split(/;\s*/);
+          for (var i = 0; i < splat.length; i++) {
             var ps = splat[i].split('=');
             var k = unescape(ps[0]);
             if (k === key) return unescape(ps[1]);
-        }
-        return undefined;
-    };
+          }
+          return undefined;
+        };
 
-    self.set = function (key, value, opts) {
-        if (!opts) opts = {};
-        var s = escape(key) + '=' + escape(value);
-        if (opts.expires) s += '; expires=' + opts.expires;
-        if (opts.path) s += '; path=' + escape(opts.path);
-        doc.cookie = s;
-        return s;
-    };
-    return self;
-};
+        self.set = function (key, value, opts) {
+          if (!opts) opts = {};
+          var s = escape(key) + '=' + escape(value);
+          if (opts.expires) s += '; expires=' + opts.expires;
+          if (opts.path) s += '; path=' + escape(opts.path);
+          doc.cookie = s;
+          return s;
+        };
+        return self;
+      };
 
-if (typeof document !== 'undefined') {
-    var cookie = exports(document);
-    exports.get = cookie.get;
-    exports.set = cookie.set;
-}
+      if (typeof document !== 'undefined') {
+        var cookie = exports(document);
+        exports.get = cookie.get;
+        exports.set = cookie.set;
+      }
 
-},{}],4:[function(require,module,exports){
-/**
- * @module  get-doc
- */
+    }, {}],
+    4: [function (require, module, exports) {
+      /**
+       * @module  get-doc
+       */
 
-var hasDom = require('has-dom');
+      var hasDom = require('has-dom');
 
-module.exports = hasDom() ? document : null;
-},{"has-dom":5}],5:[function(require,module,exports){
-'use strict';
-module.exports = function () {
-	return typeof window !== 'undefined'
-		&& typeof document !== 'undefined'
-		&& typeof document.createElement === 'function';
-};
+      module.exports = hasDom() ? document : null;
+    }, {
+      "has-dom": 5
+    }],
+    5: [function (require, module, exports) {
+      'use strict';
+      module.exports = function () {
+        return typeof window !== 'undefined' && typeof document !== 'undefined' && typeof document.createElement === 'function';
+      };
 
-},{}],6:[function(require,module,exports){
-module.exports = extend
+    }, {}],
+    6: [function (require, module, exports) {
+      module.exports = extend
 
-function extend(target) {
-    for (var i = 1; i < arguments.length; i++) {
-        var source = arguments[i]
+      function extend(target) {
+        for (var i = 1; i < arguments.length; i++) {
+          var source = arguments[i]
 
-        for (var key in source) {
+          for (var key in source) {
             if (source.hasOwnProperty(key)) {
-                target[key] = source[key]
+              target[key] = source[key]
             }
+          }
         }
-    }
 
-    return target
-}
+        return target
+      }
 
-},{}]},{},[1])(1)
+    }, {}]
+  }, {}, [1])(1)
 });
+
